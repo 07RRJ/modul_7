091535989b072b9d63d0458b1df76691b748dcab
07RRJ <rasmusrudko07@gmail.com>
2024-12-18 12:43:16 +0100

    1.3

--- index.py
+++ index.py
@@ -17,26 +17,70 @@ cars = [                    # dictionary
 ]
 
 while True:                 # make varables of the dictionary values
-    for car in cars:
+    for index,car in enumerate(cars, 1):
         make = car["make"]
         model = car["model"]
         year = car["year"]
-        print(f"{make} {model} {year}")
+        print(f"{index}) {make} {model} {year}")
 
-    make = input("new car: ")                    # add a car and its properties
-    model = input("model: ")
-    year = input("year: ")
-
-    cars.append(                    # add the new values to the list/dictionary
-        {
-            "make": make,
-            "model": model,
-            "year": year
-        }
-    )
+    new_car = input("\nwant to add a car, \"y\" to add ").lower()
+    if new_car == "y":
+        make = input("new car: ")                    # add a car and its properties
+        model = input("model: ")
+        year = input("year: ")
+        cars.append(                    # add the new values to the list/dictionary
+            {
+                "make": make,
+                "model": model,
+                "year": year
+            }
+        )
+        os.system("cls")
+        while True:
+            for index,car in enumerate(cars, 1):
+                make = car["make"]
+                model = car["model"]
+                year = car["year"]
+                print(f"{index}) {make} {model} {year}")
+            break
     
-    remove = int(input(f"do you want to remove a list item? removable 1 to {len(cars) + 1}, anything else to keep"))
+    while True:
+        try:
+            remove = int(input(f"\ndo you want to remove a car? removable 1 to {len(cars) + 1}, 0 to keep it as it is "))
+            if remove != 0 and remove > -1 and remove < len(cars):
+                cars.pop(remove - 1)
+                break
+            if remove == 0:
+                break
+        except:
+            print("plese use a \"0\" to keep it as it is and any list number to remove them")
+        os.system("cls")
+        while True:
+            for index,car in enumerate(cars, 1):
+                make = car["make"]
+                model = car["model"]
+                year = car["year"]
+                print(f"{index}) {make} {model} {year}")
+            break
     
-    cars.pop(remove)
+    change_car = input("change a car, \"y\" to change").lower()
+    if change_car == "y":
+        try:
+            car_change = input(f"which car do you want to change 1 to {len[cars] + 1}")
+            cars[car_change - 1] = {
+                "make": make,
+                "model": model,
+                "year": year
+            }
+        except:
+            print("use a valid number")
+        os.system("cls")
+        while True:
+            for index,car in enumerate(cars, 1):
+                make = car["make"]
+                model = car["model"]
+                year = car["year"]
+                print(f"{index}) {make} {model} {year}")
+            break
 
     os.system("cls")
